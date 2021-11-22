682b45341a4fa14627da4861a4a8690dc411b337
Luis Felipe Murguia Ramos <65832922+seel-channel@users.noreply.github.com>
2021-11-21 23:23:48 -0700

    Update train_mask_rcnn.py

--- train_mask_rcnn.py
+++ train_mask_rcnn.py
@@ -14,7 +14,7 @@ import shutil
 import zipfile
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("/content/Mask_RCNN")
+ROOT_DIR = os.path.abspath("/content/AMPD_Mask_RCNN")
 print("VERS 0.2")
 
 # Import Mask RCNN
